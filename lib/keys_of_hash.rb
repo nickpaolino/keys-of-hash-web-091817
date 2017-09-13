b556d83026b93353fcc8586b70b98794c00bfb65
github username <github email address>
2017-09-12 20:19:47 -0400

    Finished writing the new Hash class method, keys_of

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    array = []
+    args.each do |arg|
+      if self.values.include?(arg)
+        hashes = self.select {|key, value| value == arg}
+        hashes.each do |key, value|
+          array << key
+        end
+      else
+      end
+    end
+    array
   end
 end
